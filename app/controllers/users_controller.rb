70bd60973ef88c4121aaf21a0ce81f1da23e5cb6
Xavier MALPARTY <xavier@nimblehq.co>
2022-07-15 17:04:35 +0700

    Connect all turbo links together for smoother UX

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
+    redirect_to controller:'home', action:'index', notice: 'User was successfully destroyed.'
   end
 
   private
